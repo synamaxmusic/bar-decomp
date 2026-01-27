87d01ccbb2c770a6b427be99e4947d29f9bd616f
SynaMax <djsynamax@gmail.com>
2026-01-26 19:00:22 -0500

    Documented car colors
    These were found in the "caranim" module.

---
+++ include/caranim.h
@@ -0,0 +1,95 @@
+colorsEN CarColorsEN[] = {
+        "RED",
+        "YELLOW",
+        "BLUE",
+        "GREEN",
+        "ORANGE",
+        "BLACK",
+        "SILVER",
+        "GRAPE",
+        "BABY BLUE",
+        "RACING GREEN",
+        "FUSCHIA",
+        "GRAY",
+        "SEA SPRAY",
+        "BRONZE",
+        "PINK",
+        "SKY",
+        "BURGUNDY",
+        "PURPLE",
+        "MIDNIGHT BLUE",
+        "SPEARMINT",
+        "CHARCOAL"
+};
+
+colorsDE CarColorsDE[] = {
+        "ROT",
+        "GELB",
+        "BLAU",
+        "GR}N",
+        "ORANGE",
+        "SCHWARZ",
+        "SILBER",
+        "BORDEAUX",
+        "HELLBLAU",
+        "GRASGR}N",
+        "PINK",
+        "GRAU",
+        "PETROL",
+        "BRONZE",
+        "ROSA",
+        "HIMMELBLAU",
+        "BURGUND",
+        "LILA",
+        "NACHTBLAU",
+        "MINTGR}N",
+        "SCHWARZ"
+};
+
+colorsFR CarColorsFR[] = {
+        "ROUGE",
+        "JAUNE",
+        "BLEU",
+        "VERT",
+        "ORANGE",
+        "NOIR",
+        "GRIS METAL",
+        "LIE-DE-VIN",
+        "BLEU PALE",
+        "VERT BOUTEILLE",
+        "FUCHSIA",
+        "GRIS",
+        "BLEU LAGON",
+        "BRONZE",
+        "ROSE",
+        "BLEU CIEL",
+        "BORDEAUX",
+        "VIOLET",
+        "BLEU NUIT",
+        "MENTHE VERTE",
+        "ANTHRACITE"
+};
+
+colors CarColorHex[] = {
+        {FF,20,20,00}, // RED
+        {FF,FF,20,00}, // YELLOW
+        {30,26,FF,00}, // BLUE
+        {B0,FF,50,00}, // GREEN
+        {FF,98,20,00}, // ORANGE
+        {20,20,20,00}, // BLACK
+        {FF,FF,FF,00}, // SILVER
+        {32,10,32,00}, // GRAPE (dark purple)
+        {89,96,FF,00}, // BABY BLUE
+        {10,4B,10,00}, // RACING GREEN
+        {FF,10,96,00}, // FUSCHIA (hot pink)
+        {96,96,96,00}, // GRAY
+        {10,B4,6E,00}, // SEA SPRAY (greenish-blue)
+        {82,78,69,00}, // BRONZE
+        {FF,64,FF,00}, // PINK
+        {10,96,FF,00}, // SKY
+        {50,10,14,00}, // BURGUNDY
+        {96,10,FF,00}, // PURPLE
+        {10,10,64,00}, // MIDNIGHT BLUE
+        {32,FF,FF,00}, // SPEARMINT
+        {50,46,64,00}  // CHARCOAL
+};
