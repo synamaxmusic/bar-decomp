6dc81f0ea73fc56fb9d3784bfea56034c26b9f3a
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-04-18 01:36:35 +0000

    `Splash_Exports`

--- include/splash.h
+++ include/splash.h
@@ -3,9 +3,10 @@
 #define BAR_SPLASH_H
 
 typedef struct Splash_Exports_s {
-    /* 0x00 */ void (*PLACEHOLDER)(void);                      /* inferred */
-    /* 0x04 */ void (*PLACEHOLDER)(void);
-    /* 0x08 */ void (*PLACEHOLDER)(void);
+    /* 0x00 */ void (*func_splash_004005F8)(void);                      /* inferred */
+    /* 0x04 */ void (*func_splash_00400000)(void);
+    /* 0x08 */ void (*func_splash_0040020C)(void);
+    /* 0x0C */ void (*func_splash_00400828)(void);
 } Splash_Exports;
 
 #endif /* BAR_SPLASH_H */
